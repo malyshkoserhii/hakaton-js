b492ae4fbf60306fda2f4632a1beb2fe4e1f7ae7
Malyshko Serhii <malyshkosergiy@gmail.com>
2020-10-28 18:41:50 +0200

    routing practice has been finished

--- src/js/routing-wq.js
+++ src/js/routing-wq.js
@@ -1,14 +1,24 @@
 import watch from '../templates/watch-list.hbs';
 import queue from '../templates/queue-list.hbs';
+import { createRouter } from 'routerjs';
 
 const mainRef = document.querySelector('main');
 
 function createWatchMarkup() {
   const markupWatch = watch();
-  mainRef.insertAdjacentHTML('beforeend', markupWatch);
+  mainRef.innerHTML = markupWatch;
 }
 
 function createQueueMarkup() {
   const queueMarkup = queue();
-  mainRef.insertAdjacentHTML('beforeend', queueMarkup);
+  mainRef.innerHTML = queueMarkup;
 }
+
+const router = createRouter()
+  .get('/watch', (req, context) => {
+    createWatchMarkup();
+  })
+  .get('/queue', (req, context) => {
+    createQueueMarkup();
+  })
+  .run();
