7571997cf02b898f7f2df3938f09534ec8f77775
Malyshko Serhii <malyshkosergiy@gmail.com>
2020-10-30 22:41:15 +0200

    fixed styles issues; localstorage feature has been added

--- src/js/routing-wq.js
+++ src/js/routing-wq.js
@@ -2,7 +2,23 @@ import watch from '../templates/watch-list.hbs';
 import queue from '../templates/queue-list.hbs';
 import { createRouter } from 'routerjs';
 
+const watchLinkRef = document.querySelector('.watch-js');
+const queueLinkRef = document.querySelector('.queue-js');
 const libraryRef = document.querySelector('.library');
+
+savedChoice();
+
+const router = createRouter()
+  .get('/watch', (req, context) => {
+    createWatchMarkup();
+  })
+  .get('/queue', (req, context) => {
+    createQueueMarkup();
+  })
+  .run();
+
+watchLinkRef.addEventListener('click', focusWatch);
+queueLinkRef.addEventListener('click', focusQueue);
 
 function createWatchMarkup() {
   const markupWatch = watch();
@@ -14,11 +30,23 @@ function createQueueMarkup() {
   libraryRef.innerHTML = queueMarkup;
 }
 
-const router = createRouter()
-  .get('/watch', (req, context) => {
-    createWatchMarkup();
-  })
-  .get('/queue', (req, context) => {
-    createQueueMarkup();
-  })
-  .run();
+function focusWatch() {
+  watchLinkRef.classList.add('is-active');
+  queueLinkRef.classList.remove('is-active');
+  localStorage.setItem('focused', 'watch');
+}
+
+function focusQueue() {
+  watchLinkRef.classList.remove('is-active');
+  queueLinkRef.classList.add('is-active');
+  localStorage.setItem('focused', 'queue');
+}
+
+function savedChoice() {
+  const saved = localStorage.getItem('focused');
+
+  if (saved === 'queue') {
+    queueLinkRef.classList.add('is-active');
+    watchLinkRef.classList.remove('is-active');
+  }
+}
